dde1c044ff734f3f5233b45d0105daa3308a2bb1
Christopher Lillthors <christopher.lillthors@gmail.com>
2014-10-17 22:17:06 +0200

    Added gograph

--- GoGraph.go
+++ GoGraph.go
@@ -20,7 +20,19 @@ func init() {
 
 func main() {
 	r := gin.Default()
+	r.Use(CORSMiddleware())
 
+	public := r.Group("/api")
+
+	public.POST("/login", func(c *gin.Context) {
+
+	})
+
+	private := r.Group("/api/auth", tokenMiddleWare("unicornsareawesome"))
+
+	private.POST("/dijkstras", func(c *gin.Context) {
+
+	})
 	listeningAdrs := adress + ":" + port
 	r.Run(listeningAdrs)
 }
---
+++ token.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	jwt_lib "github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+// Gin middleware. Checks for valid tokens in the http auth header.
+func tokenMiddleWare(secret string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		_, err := jwt_lib.ParseFromRequest(c.Request, func(token *jwt_lib.Token) (interface{}, error) {
+			return secret, nil
+		})
+		if err != nil {
+			c.Fail(401, err)
+		}
+	}
+}
+
+// Generate a valid token. Put this is in the auth header when making calls to auth routes.
+func generateToken(secret []byte, claims *map[string]interface{}) (string, error) {
+	token := jwt_lib.New(jwt_lib.GetSigningMethod("HS256"))
+	token.Claims = *claims
+	tokenString, err := token.SignedString(secret)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
